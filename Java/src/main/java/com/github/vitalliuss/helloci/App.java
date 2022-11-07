41ce279eae077bbc9082bdbd3bddc27b91155053
@sev4kz <vsevolod_trusov_00@mail.ru>
2022-11-07 16:37:23 +0300

    test

--- Java/src/main/java/com/github/vitalliuss/helloci/App.java
+++ Java/src/main/java/com/github/vitalliuss/helloci/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
+        Calculator calculator = new Calculator();
         System.out.println( "Hello CI!" );
+        System.out.println( "2 + 3 = " + calculator.getAmount(2, 3) );
+        System.out.println( "2 - 3 = " + calculator.getDifference(2, 3) );
+        System.out.println( "2 * 3 = " + calculator.getProduct(2, 3) );
+        System.out.println( "2 / 3 = " + calculator.getQuotient(2, 3) );
+        System.out.println( "2 % 3 = " + calculator.getRemainder(2, 3) );
+
     }
 }
